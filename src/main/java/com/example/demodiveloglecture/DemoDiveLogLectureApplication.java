35ee3cfe57698e96e36f6b45b5de8c5527a95099
eatnows <gagyasindev@gmail.com>
2023-01-17 21:53:56 +0900

    initial commit

---
+++ src/main/java/com/example/demodiveloglecture/DemoDiveLogLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodiveloglecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDiveLogLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDiveLogLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demodiveloglecture/DemoDiveLogLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodiveloglecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiveLogLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
